5c23c302a6587eda2e4c050324219ba1008fcbad
Kirill <kirill.polzounov@ucalgary.ca>
2018-06-15 17:07:31 -0700

    Added support for 2 inverted pendulum tasks

--- gym-enviroments/gym_qube/__init__.py
+++ gym-enviroments/gym_qube/__init__.py
@@ -1,6 +1,11 @@
 from gym.envs.registration import register
 
 register(
-    id='Qube-v0',
-    entry_point='gym_qube.envs:QubeEnv',
+    id='Qube-Inverted-Pendulum-v0',
+    entry_point='gym_qube.envs:QubeInvertedPendulumEnv',
+)
+
+register(
+    id='Qube-Inverted-Pendulum-Sparse-v0',
+    entry_point='gym_qube.envs:QubeInvertedPendulumSparseRewardEnv',
 )
