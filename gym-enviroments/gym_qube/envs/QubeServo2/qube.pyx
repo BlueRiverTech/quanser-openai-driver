178992e94adf10ac3564f101016d2233ba1635cc
Kirill Polzounov <kirill.polzounov@ucalgary.ca>
2018-06-18 11:24:49 -0700

    Fixed import bug

--- gym-enviroments/gym_qube/envs/QubeServo2/qube.pyx
+++ gym-enviroments/gym_qube/envs/QubeServo2/qube.pyx
@@ -6,7 +6,7 @@ cimport quanser_types as qt
 cimport numpy as np
 cimport hil
 
-from helpers.error_codes import print_possible_error
+from gym_qube.envs.QubeServo2.helpers.error_codes import print_possible_error
 import numpy as np
 import time
 
