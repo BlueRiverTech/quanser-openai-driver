3acbd5a995fe738e161b759bb4cd7efae5a812f1
Kirill Polzounov <kirill.polzounov@ucalgary.ca>
2018-10-25 21:49:51 -0600

    Modified QubeBeginUprightEnv to be similar to OpenAI CartPole

--- gym_brt/envs/qube_begin_upright_env.py
+++ gym_brt/envs/qube_begin_upright_env.py
@@ -18,32 +18,56 @@ class QubeBeginUprightReward(object):
             high=ACTION_HIGH, dtype=np.float32)
 
     def __call__(self, state, action):
-        theta_x = state[0]
-        theta_y = state[1]
-        alpha_x = state[2]
-        alpha_y = state[3]
-        theta_velocity = state[4]
-        alpha_velocity = state[5]
-        theta_acceleration = state[6]
-        alpha_acceleration = state[7]
-
-        theta = np.arctan2(theta_y, theta_x)  # arm
-        alpha = np.arctan2(alpha_y, alpha_x)  # pole
-
-        cost = normalize_angle(theta)**4 + \
-            normalize_angle(alpha)**2 + \
-            0.1 * alpha_velocity**2
-
-        reward = -cost
+        reward = 1
         return reward
 
 
 class QubeBeginUprightEnv(QubeBaseEnv):
+    """
+    Description:
+        A pendulum is attached to an un-actuated joint to a horizontal arm, 
+        which is actuated by a rotary motor. The pendulum begins 
+        upright/inverted and the goal is to keep it from falling by applying a
+        voltage on the motor which causes a torque on the horizontal arm.
+
+    Source:
+        This is modified for the Quanser Qube Servo2-USB from the Cart Pole
+        problem described by Barto, Sutton, and Anderson, and implemented in 
+        OpenAI Gym: https://github.com/openai/gym/blob/master/gym/envs/classic_control/cartpole.py
+        This description is also modified from the description by the OpenAI
+        team.
+
+    Observation: 
+        Type: Box(4)
+        Num Observation                 Min         Max
+        0   Arm Angle (theta)         -10.0           10.0
+        1   Cart Velocity             -Inf            Inf
+        2   Pole Angle                -90.0°          90.0°
+        3   Pole Velocity At Tip      -Inf            Inf
+
+        Note: the velocities are limited by the physical system.
+        
+    Actions:
+        Type: Continuous (voltage applied to motor)
+
+    Reward:
+        Reward is 1 for every step taken, including the termination step
+
+    Starting State:
+        Use a classical controller to get the pendulum into it's initial
+        inverted state. This has inherent randomness.
+
+    Episode Termination:
+        Pendulum Angle (alpha) is more than ±10° from upright
+        Arm Angle (theta) is more than ±90° from start point
+        Episode length is greater than 2000
+    """
     def __init__(self, frequency=1000, use_simulator=False):
         super(QubeBeginUprightEnv, self).__init__(
             frequency=frequency,
             use_simulator=use_simulator)
         self.reward_fn = QubeBeginUprightReward()
+        self._total_steps = 0
 
     def reset(self):
         # Start the pendulum stationary at the top (stable point)
@@ -52,10 +76,25 @@ class QubeBeginUprightEnv(QubeBaseEnv):
 
     def _done(self):
         # The episode ends whenever the angle alpha is outside the tolerance
-        return self._alpha > (10 * np.pi / 180)
+        return abs(self._alpha) > (10 * np.pi / 180) 
+            or abs(self._theta) > (90 * np.pi / 180)
+            or self._total_steps > 2000
 
     def step(self, action):
+        self._total_steps += 1
         state, reward, _, info = super(QubeBeginUprightEnv, self).step(action)
+
+        # Get the angles
+        theta_x, theta_y = state[0], state[1]
+        alpha_x, alpha_y = state[2], state[3]
+        theta = np.arctan2(theta_y, theta_x)
+        alpha = np.arctan2(alpha_y, alpha_x)
+        theta_dot = state[4]
+        alpha_dot = state[5]
+
+        # Make the state simpler/closer to cartpole
+        state = np.asarray([theta, alpha, theta_dot, alpha_dot])
+
         done = self._done()
         return state, reward, done, info
 
