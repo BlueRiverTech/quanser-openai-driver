1c82d93679f09dfdf397adefa4187a4488f1ca37
Kirill <kirill.polzounov@ucalgary.ca>
2019-08-23 08:11:51 -0600

    Fixed follow test envs

--- gym_brt/envs/qube_test_env.py
+++ gym_brt/envs/qube_test_env.py
@@ -5,6 +5,10 @@ from __future__ import division
 import numpy as np
 from gym import spaces
 from gym_brt.envs.qube_base_env import QubeBaseEnv
+from gym_brt.envs.qube_swingup_env import QubeSwingupFollowEnv
+from gym_brt.envs.qube_balance_env import QubeBalanceFollowEnv
+from gym_brt.envs.qube_dampen_env import QubeDampenFollowEnv
+from gym_brt.envs.qube_rotor_env import QubeRotorFollowEnv
 
 
 """
@@ -13,77 +17,25 @@ the `Follow` environments.
 """
 
 
-class QubeBalanceFollowSineWaveEnv(QubeBaseEnv):
-    def _get_state(self):
-        return np.array(
-            [
-                self._theta - self._next_target_angle(),
-                self._alpha,
-                self._theta_dot,
-                self._alpha_dot,
-            ],
-            dtype=np.float64,
-        )
-
-    def _reward(self):
-        return 0
-
-    def _isdone(self):
-        return abs(self._theta) > np.pi / 2
-
-    def reset(self):
-        super(QubeBalanceFollowSineWaveEnv, self).reset()
-        state = self._reset_up()
-        return state
-
+class QubeBalanceFollowSineWaveEnv(QubeBalanceFollowEnv):
     def _next_target_angle(self):
         # Sine wave between -180 to +180 degrees
         return np.pi / 3 * np.cos(self._episode_steps / self._frequency)
 
 
-class QubeSwingupFollowSineWaveEnv(QubeBaseEnv):
-    def _get_state(self):
-        target_angle = (
-            self._next_target_angle() if self._alpha < (20 * np.pi / 180) else 0
-        )
-        return np.array(
-            [self._theta - target_angle, self._alpha, self._theta_dot, self._alpha_dot],
-            dtype=np.float64,
-        )
-
-    def _reward(self):
-        return 0
-
-    def _isdone(self):
-        return abs(self._theta) > np.pi / 2
-
-    def reset(self):
-        super(QubeSwingupFollowSineWaveEnv, self).reset()
-        state = self._reset_down()
-        return state
-
+class QubeSwingupFollowSineWaveEnv(QubeSwingupFollowEnv):
     def _next_target_angle(self):
         # Sine wave between -180 to +180 degrees
         return np.pi / 3 * np.cos(self._episode_steps / self._frequency)
 
 
-class QubeRotorFollowSineWaveEnv(QubeSwingupFollowSineWaveEnv):
-    pass
+class QubeRotorFollowSineWaveEnv(QubeRotorFollowEnv):
+    def _next_target_angle(self):
+        # Sine wave between -180 to +180 degrees
+        return np.pi / 3 * np.cos(self._episode_steps / self._frequency)
 
 
-class QubeDampenFollowSineWaveEnv(QubeBaseEnv):
-    def _reward(self):
-        return 0
-
-    def _isdone(self):
-        return False
-
-    def reset(self):
-        super(QubeBalanceFollowSineWaveEnv, self).reset()
-        state = self._reset_up()
-        time.sleep(np.random.randint(1, 3000) / 1000)  # Sleep between 1-3000ms
-        return state
-
+class QubeDampenFollowSineWaveEnv(QubeDampenFollowEnv):
     def _next_target_angle(self):
         # Sine wave between -180 to +180 degrees
         return np.pi * np.cos(self._episode_steps / self._frequency)
